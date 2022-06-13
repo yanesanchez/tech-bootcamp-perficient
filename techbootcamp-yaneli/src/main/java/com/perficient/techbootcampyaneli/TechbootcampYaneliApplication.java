4bced202059c13d31b3cb3cdc5c020904ea73fec
Yaneli Sanchez <yaneli.dev@gmail.com>
2022-06-13 18:22:43 -0400

    Added new generated Spring Boot project (Java version 17))
    start.spring.io

---
+++ techbootcamp-yaneli/src/main/java/com/perficient/techbootcampyaneli/TechbootcampYaneliApplication.java
@@ -0,0 +1,13 @@
+package com.perficient.techbootcampyaneli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechbootcampYaneliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechbootcampYaneliApplication.class, args);
+	}
+
+}
